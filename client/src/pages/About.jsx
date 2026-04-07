ece0b1dc03421258f322130d03d57dc3d8f6b749
RPMorrigan <morriganpriscilla@gmail.com>
2026-04-07 10:23:02 -0600

    Redoing my client folder

--- client/src/pages/About.jsx
+++
@@ -1,5 +0,0 @@
-// pages/About.jsx
-export default function About() {
-    return <h1>About Us</h1>;
-  }
-  
--- client/src/pages/Home.jsx
+++
@@ -1,5 +0,0 @@
-// pages/Home.jsx
-export default function Home() {
-    return <h1>Welcome to the Home Page</h1>;
-  }
-  
