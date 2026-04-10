5669ad33e2ea3bf54b77f055af265459dd363950
RPMorrigan <morriganpriscilla@gmail.com>
2026-04-10 12:07:49 -0600

    Saving while I have the minimal mobile page. About to set up buttons

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,6 +6,7 @@ import Card from '../components/Card';
 export default function Home() {
 
   const [suggestions, setSuggestions] = useState([]);
+  const [filters, setFilters] = useState([]);
 
   const fetchSuggestions = async () => {
     const response = await fetch('api/get-all-suggestions');
@@ -20,20 +21,30 @@ export default function Home() {
   }, []);
 
   return <div className="home-body">
+
+    <div className="suggestion-nav">
+      <h4>Suggestions</h4>
+      <button className="add-button big-button">+ Add Feedback</button>
+    </div>
   
-    <div className="filter-buttons">
-      <button>All</button>
-      <button>UI</button>
-      <button>UX</button>
-      <button>Enhancement</button>
-      <button>Bug</button>
-      <button>Feature</button>
+    <div className="filter-buttons-wrap">
+      <button className="filter-button button5">All</button>
+      <button className="filter-button button5">UI</button>
+      <button className="filter-button button5">UX</button>
+      <button className="filter-button button5">Enhancement</button>
+      <button className="filter-button button5">Bug</button>
+      <button className="filter-button button5">Feature</button>
     </div>
 
-    <div className="requests-wrapper">
+    <div className="request-wrapper">
       {suggestions.map(s => {
         <Card suggestion={s} />
       })}
+
+      <h2>There is no feedback yet.</h2>
+      <p className="body-txt1">Got a suggestion? Found a bug that needs to be squashed? We love hearing about new ideas to improve our app.</p>
+
+      <button className="add-button big-button button1">+ Add Feedback</button>
     </div>
 
   </div>
